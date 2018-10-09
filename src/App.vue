26decf408cac8d0b4da5dafef5a288216a15c884
Nicolás Gómez <ngomezgg.18@gmail.com>
2018-10-09 18:54:36 -0500

    Create Header component

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,16 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Recipes"/>
+    <Header />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import Header from './components/Header.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld,
+    Header
   },
 };
 </script>
-
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,53 @@
+<template>
+    <section class="hero is-info">
+    <div class="hero-head">
+      <nav class="navbar">
+        <div class="container">
+          <div class="navbar-brand">
+            <a class="navbar-item">
+              <img src="https://bulma.io/images/bulma-type-white.png" alt="Logo">
+            </a>
+          </div>
+          <div id="navbarMenuHeroB" class="navbar-menu">
+            <div class="navbar-end">
+              <span class="navbar-item">
+                <a class="button is-info is-inverted" href="http://github.com/ngomez22/recipes-front">
+                  <span class="icon">
+                    <i class="fab fa-github"></i>
+                  </span>
+                  <span>View source</span>
+                </a>
+              </span>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+
+    <div class="hero-body">
+      <div class="container has-text-centered">
+        <p class="title">
+          Recipes
+        </p>
+        <p class="subtitle">
+          by Nicolás Gómez
+        </p>
+      </div>
+    </div>
+
+    <div class="hero-foot">
+      <nav class="tabs is-boxed is-fullwidth">
+        <div class="container">
+          <ul>
+            <li class="is-active">
+              <a>View Recipes</a>
+            </li>
+            <li>
+              <a>Create Recipe</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  </section>
+</template>
